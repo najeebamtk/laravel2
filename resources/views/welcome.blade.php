c9c54cf1f773b3aafab30e815af69e1c498bbc58
najeebamtk <najeebamtk1995@gmail.com>
2021-11-29 12:29:20 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>fara</h1>
+    <a href="{{url('/')}}">
+        HOME<a>
+        <a href="{{url('aboutus')}}">
+        ABOUT<a>        
+        <a href="{{url('contact')}}">
+        CONTACT<a>
+</body>
+</html>
